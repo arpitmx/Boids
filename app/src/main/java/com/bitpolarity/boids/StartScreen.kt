18633f717dd4ea0d2a39754da579b99aacc6579c
Armax | Alok <arpitmaurya01codechef@gmail.com>
2023-12-28 02:45:17 +0530

    Stability improvements, Ui Updates, Boid radius seekbar added

--- app/src/main/java/com/bitpolarity/boids/StartScreen.kt
+++ app/src/main/java/com/bitpolarity/boids/StartScreen.kt
@@ -1,11 +1,54 @@
 package com.bitpolarity.boids
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.bitpolarity.boids.ExtensionsUtil.animFadein
+import com.bitpolarity.boids.ExtensionsUtil.blink
+import com.bitpolarity.boids.databinding.ActivityStartScreenBinding
 
 class StartScreen : AppCompatActivity() {
+
+    private val binding: ActivityStartScreenBinding by lazy {
+        ActivityStartScreenBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start_screen)
+        hideSystemUI()
+        setContentView(binding.root)
+        setUpViews()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+    }
+
+
+    private fun setUpViews() {
+        binding.boidLogo.animFadein(this,1500)
+        binding.byarmaxlogo.animFadein(this,1000)
+        startTimer()
+    }
+
+    private fun startTimer() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finishAffinity()
+        },3000)
     }
 }
